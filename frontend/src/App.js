58fd26e8e982abc4f9c8749c69341851cb921db9
abarreracon <abarreracon@gmail.com>
2021-04-25 19:02:38 -0500

    building the cart base feature

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,7 +14,7 @@ function App() {
         <Container>
           <Route path="/" component={HomeScreen} exact />
           <Route path="/product/:id" component={ProductScreen} />
-          <Route path="/cart/:id?" component={ProductScreen} />
+          <Route path="/cart/:id?" component={CartScreen} />
         </Container>
       </main>
 
