ebe39cac05adc900ab1315fae1645cc093d26cc0
abarreracon <abarreracon@gmail.com>
2021-05-09 17:33:57 -0500

    creating LoginScreen 2

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -13,24 +13,23 @@ export const login = (email, password) => async (dispatch) => {
     });
 
     const config = {
-        headers:{
-            'Content-type': 'application/json'
-        }
-    }
+      headers: {
+        "Content-type": "application/json",
+      },
+    };
 
     const { data } = await axios.post(
-        "/api/users/login/"
-        {'username': email, 'password': password},
-        config
-        );
+      "/api/users/login/",
+      { username: email, password: password },
+      config
+    );
 
     dispatch({
-        type: USER_LOGIN_SUCCESS,
-        payload:data
-    })
+      type: USER_LOGIN_SUCCESS,
+      payload: data,
+    });
 
-    localStorage.setItem('userInfo', JSON.stringify(data))
-
+    localStorage.setItem("userInfo", JSON.stringify(data));
   } catch (error) {
     dispatch({
       type: USER_LOGIN_FAIL,
